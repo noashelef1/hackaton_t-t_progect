65c5fe2fa027d706311f0ca25b036dffe1cdc77d
noa <noa.shelef1@gmail.com>
2025-08-27 14:24:11 +0300

    filtered_research_function

---
+++ missions.py
@@ -0,0 +1,10 @@
+def research_filter(missions_details_dict, users_filter_dict):
+    missions_filtered_list = []
+
+    for mission in missions_details_dict:
+        for details in mission:
+            if details in users_filter_dict.keys() and mission not in missions_filtered_list:
+                missions_filtered_list.append(mission)
+
+    return missions_filtered_list
+
